b975e7dff7ca282d258a9ff49bb04d5b5c12b394
venka <venka@10.0.0.228>
2024-02-13 20:22:18 -0500

    Gday1learning

---
+++ Day1/src/Day1learning/JavaBasics.java
@@ -0,0 +1,26 @@
+package Day1learning;
+
+public class JavaBasics 
+{
+
+	public static void main(String[] args) 
+	{
+		
+       System.out.println("This is my first Program");
+       
+       int mynum = 5;
+       System.out.println(mynum+" this is the value");
+       
+       String name = "hemanth";
+       System.out.println(name);
+       
+       char letter='H';
+       System.out.println(letter);
+       
+       double deci=6.49;
+       System.out.println(deci);
+       
+       
+	}
+
+}
